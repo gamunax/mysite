25c6609ee94ef61a04cbf3ed5faf704b90c9e0e9
Jan Pierre <gamunaxx@gmail.com>
2022-02-23 09:29:02 -0500

    feat: add target

--- src/App.js
+++ src/App.js
@@ -10,22 +10,22 @@ function App() {
         <p>
           Senior Developer                   
         </p>
-        <a href="https://github.com/gamunax">
+        <a href="https://github.com/gamunax" target="_blank">
           <i class="icon fab fa-github fa-2x"></i>
         </a>
-        <a href="https://medium.com/@janpierrsanchez/">
+        <a href="https://medium.com/@janpierrsanchez/" target="_blank">
           <i class="icon fab fa-brands fa-medium fa-2x"></i>
         </a>
-        <a href="https://www.linkedin.com/in/jose-jan-pierre-sanchez-manosalva/">
+        <a href="https://www.linkedin.com/in/jose-jan-pierre-sanchez-manosalva/" target="_blank">
           <i class="icon fab fa-linkedin-in fa-2x"></i>
         </a>
-        <a href="https://es.stackoverflow.com/users/14040/jan-pierre-sanchez">
+        <a href="https://es.stackoverflow.com/users/14040/jan-pierre-sanchez" target="_blank">
           <i class="icon fab fa-stack-overflow fa-2x"></i>
         </a>
-        <a href="https://www.facebook.com/groups/dev.angular">
+        <a href="https://www.facebook.com/groups/dev.angular" target="_blank">
           <i class="icon fab fa-facebook fa-2x"></i>
         </a>
-        <a href="mailto:gamunaxx@gmail.com">
+        <a href="mailto:gamunaxx@gmail.com" target="_blank">
           <i class="icon fas fa-envelope fa-2x"></i>
         </a>
       </div>
